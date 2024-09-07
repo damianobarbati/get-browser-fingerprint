a3b1763b1ee999db7af735584fb9eec49b8277f7
damiano.barbati <damiano.barbati@gmail.com>
2024-09-07 18:44:18 +0200

    fix pup install in CI

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import { chromium } from "@playwright/test";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 describe("getBrowserFingerprint", () => {
@@ -6,16 +6,9 @@ describe("getBrowserFingerprint", () => {
   let page;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      // headless: false,
-      // devtools: true,
-    });
+    browser = await chromium.launch();
     page = await browser.newPage();
-
-    await page.addScriptTag({
-      type: "module",
-      path: "./src/index.js",
-    });
+    await page.addScriptTag({ path: "./src/index.js", type: "module" }); // add the script to the page
   });
 
   afterAll(async () => {
@@ -24,18 +17,16 @@ describe("getBrowserFingerprint", () => {
 
   it("works without args", async () => {
     const result = await page.evaluate(() => {
-      const result = window.getBrowserFingerprint();
-      return result;
+      return window.getBrowserFingerprint();
     });
 
     expect(typeof result).toBe("number");
     expect(String(result).length).toBeGreaterThanOrEqual(7);
   });
 
-  it("works without hardwareOnly=true", async () => {
+  it("works with hardwareOnly=true", async () => {
     const result = await page.evaluate(() => {
-      const result = window.getBrowserFingerprint();
-      return result;
+      return window.getBrowserFingerprint({ hardwareOnly: true });
     });
 
     expect(typeof result).toBe("number");
@@ -44,8 +35,7 @@ describe("getBrowserFingerprint", () => {
 
   it("works with enableWebgl=true", async () => {
     const result = await page.evaluate(() => {
-      const result = window.getBrowserFingerprint({ enableWebgl: true });
-      return result;
+      return window.getBrowserFingerprint({ enableWebgl: true });
     });
 
     expect(typeof result).toBe("number");
